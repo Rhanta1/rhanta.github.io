c1f245b414ba6f0ed927ce851d07fd7af6b424b6
Rhanta <131491085+Rhanta1@users.noreply.github.com>
2024-10-09 22:32:52 +0300

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,23 @@
+
+const config = {
+    profileImage: "image/1.gif",
+    userName: "Rhanta",
+    bioText: "Merhaba Ben Rhanta, Basit Bir Python Developereyım Basit Ve İşlevli Kodlar Yazarım",
+    spotifyEmbedUrl: "https://open.spotify.com/embed/track/2wBnZdVWa5jVpvYRfGU7rP?utm_source=generator",
+    socialLinks: {
+        spotify: "https://open.spotify.com/embed/track/2wBnZdVWa5jVpvYRfGU7rP?utm_source=generator",
+        instagram: "",
+        youtube: "https://www.youtube.com/@Rhanta",
+        github: "https://github.com/Rhanta1",
+        discord: "rhanta.",
+        custom: "https://cheatglobal.com/members/rhanta.315350/"
+    },
+    customIcon: "image/cg.png",
+    createdBy: {
+        text: "	Rhanta",
+        link: "https://acaroguzhan.com/"
+    },
+    backgroundImage: "image/4.gif",
+    usernameColor: "#4b0082",
+    usernameShadowColor: "#4b0082"
+};
